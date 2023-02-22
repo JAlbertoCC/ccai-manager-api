095cdbecdbc067ec76be19f14f401e375dc19542
Oscar Campos <camposhernandezoscarfco@gmail.com>
2023-02-22 15:25:46 -0600

    add call for procedure store

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -12,6 +12,20 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+const registerUsers = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    console.log(req)
+    // const result = await connection.query("CALL user_registration(?,?,?,?,?,?,?,?,?,?,?,)");
+    console.log(result)
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
 export const methods = {
-  getAllUsers
+  getAllUsers,
+  registerUsers
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,5 +3,5 @@ import { methods as userController } from "../controllers/users.controller";
 const router = Router();
 
 router.get("/api/users", userController.getAllUsers)
-
+router.get("/api/register-users", userController.registerUsers)
 export default router;
