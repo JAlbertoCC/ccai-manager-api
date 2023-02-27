15075d35e763bc126d412896b85d4d801110f56c
marcosdgp <46471081+marcosdgp@users.noreply.github.com>
2023-02-27 13:25:04 -0600

    Changes to the password and the local

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import languajeRoutes from "./routes/users.routes";
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 // Middlewares
 /*Funciones intermedias entre una petición y una respuesta*/
