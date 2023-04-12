76f1b3456757419a3d81372dd33bf961047cc346
miguel <miguel.olveramc@gmial.com>
2023-04-12 14:53:01 -0600

    "cambios de hoy en back end"

--- src/network/network.js
+++ src/network/network.js
@@ -13,6 +13,16 @@ myHeaders.append("Content-Type", "application/json");
 // esta es la mamada que estoy haciendo yo
 // va a recibir dos cosas el json y la uri 
 export function buildPostRequestMail(jsonEmail){
+     return new Request( `${urlSenGrid}`),{
+          
+               method: 'POST',
+               headers: myHeaders,
+               body: jsonEmail, //raw
+               redirect: 'follow'
+               
+            } 
+
+     }
 /*
     return new Request({
     }, {
@@ -21,9 +31,12 @@ export function buildPostRequestMail(jsonEmail){
         body: jsonEmail, //raw
         redirect: 'follow'
      } )
-*/
-     fetch("http://localhost:3001/api/send-email", requestOptions)
+
+
+          fetch("http://localhost:3001/api/send-email", requestOptions)
      .then(response => response.text())
      .then(result => console.log(result))
      .catch(error => console.log('error', error));
-}
+
+*/
+
--- src/routes/sendGrid.routes.js
+++ src/routes/sendGrid.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 // esta linea
 import { methods as sangridController } from "../controllers/sendgrid.controller";
 const router = Router();
-router.post("/api/send-email",sangridController.sendEmailAcceptOrRefuse);
+router.post("api/send-email", sangridController.sendEmailAcceptOrRefuse);
 // router.post("https://api.sendgrid.com/v3/mail/send",sangridController.sendEmailAcceptOrRefuse);
-
+// y ella bien bichi bichi
 export default router;
