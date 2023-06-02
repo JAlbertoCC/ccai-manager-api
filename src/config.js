4e5a242ab61e67f937714a3977d5bb17c227899c
Sandy Cortes <sandra_hakufu@hotmail.com>
2023-06-01 20:12:13 -0600

    update config

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import { config } from "dotenv";
 config();
 
 export default {
-  host: process.env.HOST || "192.168.166.64",
-  database: process.env.DATABASE || "sg_ccai_manager",
-  user: process.env.USER || "Servidor-CCAI",
-  password: process.env.PASSWORD || "ccai"
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD
 }
