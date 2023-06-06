0ea1180ba6d66d78eb3c6f23a19e9c2e53071dfd
Jona458 <ja717215@gmail.com>
2023-06-06 12:28:22 -0600

    add conexion db

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@ import { config } from "dotenv";
 config();
 
 export default {
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD
+  host: process.env.HOST || "192.168.166.64",
+  database: process.env.DATABASE || "sg_ccai_manager",
+  user: process.env.USER || "Servidor-CCAI",
+  password: process.env.PASSWORD || "ccai"
 }
